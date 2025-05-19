f74450e478e752a7e780de83f8f77446cb81fb61
Leander Rodrigues <me@leander.xyz>
2025-05-19 02:42:18 -0400

    🤔 begin conversion of ntd to svelte kit app

---
+++ del/src/routes/top/+page.svelte
@@ -0,0 +1,174 @@
+<div class="content">
+  <div class="inner">
+    <h2>Top 10 Stores</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <td>Photo</td>
+          <td>Ranking</td>
+          <td>Name</td>
+          <td>Reviews</td>
+          <td>Average Rating</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <a href="/stores/salmon-house"
+              ><img
+                width="200"
+                src="/images/photos/salmon.jpeg"
+                alt="Salmon House"
+              /></a
+            >
+          </td>
+          <td>1</td>
+          <td><a href="/stores/salmon-house">Salmon House</a></td>
+          <td>3</td>
+          <td>4.3 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/toast-and-co"
+              ><img
+                width="200"
+                src="/images/photos/toast.jpg"
+                alt="Toast &amp; Co"
+              /></a
+            >
+          </td>
+          <td>2</td>
+          <td><a href="/stores/toast-and-co">Toast &amp; Co</a></td>
+          <td>3</td>
+          <td>4 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/sweet-rolls-bakery"
+              ><img
+                width="200"
+                src="/images/photos/rolls.jpg"
+                alt="Sweet Rolls Bakery"
+              /></a
+            >
+          </td>
+          <td>3</td>
+          <td><a href="/stores/sweet-rolls-bakery">Sweet Rolls Bakery</a></td>
+          <td>3</td>
+          <td>4 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/oyster-haven"
+              ><img
+                width="200"
+                src="/images/photos/oysters.jpeg"
+                alt="Oyster Haven"
+              /></a
+            >
+          </td>
+          <td>4</td>
+          <td><a href="/stores/oyster-haven">Oyster Haven</a></td>
+          <td>2</td>
+          <td>4 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/fluffy-pancake-house"
+              ><img
+                width="200"
+                src="/images/photos/pancake.jpg"
+                alt="Fluffy Pancake House"
+              /></a
+            >
+          </td>
+          <td>5</td>
+          <td>
+            <a href="/stores/fluffy-pancake-house">Fluffy Pancake House</a>
+          </td>
+          <td>3</td>
+          <td>3.7 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/pasta-paradiso"
+              ><img
+                width="200"
+                src="/images/photos/pasta.jpg"
+                alt="Pasta Paradiso"
+              /></a
+            >
+          </td>
+          <td>6</td>
+          <td><a href="/stores/pasta-paradiso">Pasta Paradiso</a></td>
+          <td>2</td>
+          <td>3.5 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/artisan-loaf"
+              ><img
+                width="200"
+                src="/images/photos/loaf.jpg"
+                alt="Artisan Loaf"
+              /></a
+            >
+          </td>
+          <td>7</td>
+          <td><a href="/stores/artisan-loaf">Artisan Loaf</a></td>
+          <td>3</td>
+          <td>3.3 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/the-charcuterie-board"
+              ><img
+                width="200"
+                src="/images/photos/charcuterie.jpg"
+                alt="The Charcuterie Board"
+              /></a
+            >
+          </td>
+          <td>8</td>
+          <td>
+            <a href="/stores/the-charcuterie-board">The Charcuterie Board</a>
+          </td>
+          <td>3</td>
+          <td>3 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/the-gourmet-burger"
+              ><img
+                width="200"
+                src="/images/photos/burger.jpg"
+                alt="The Gourmet Burger"
+              /></a
+            >
+          </td>
+          <td>9</td>
+          <td><a href="/stores/the-gourmet-burger">The Gourmet Burger</a></td>
+          <td>2</td>
+          <td>3 / 5</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/stores/sweet-treats-donuts"
+              ><img
+                width="200"
+                src="/images/photos/donut.jpg"
+                alt="Sweet Treats Donuts"
+              /></a
+            >
+          </td>
+          <td>10</td>
+          <td>
+            <a href="/stores/sweet-treats-donuts">Sweet Treats Donuts</a>
+          </td>
+          <td>3</td>
+          <td>3 / 5</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
