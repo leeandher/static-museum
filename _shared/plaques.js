1f4fd1f061bc65b68657771aa90aef4ad79d730d
Leander Rodrigues <me@leander.xyz>
2026-02-23 14:12:45 -0500

    ✏️ copy for v1/v0

--- _shared/plaques.js
+++ _shared/plaques.js
@@ -1,50 +1,50 @@
 function attachPlaque() {
   const allPlaqueData = {
     v0: {
-      title: "v0: A Structured Joke",
+      title: "A Structured Joke",
       date: "2018",
       description:
-        "Pretty self-deprecating, but that was my humour at the time I suppose. I remember being very proud of the mobile nav hamburger animation, it made its way to v2 as well. This didn't used to have projects, but I decided to put some high-school projects here along with the first webpage I ever authored.",
+        "It's pretty self-deprecating, but honestly, that was accurate to my humour at the time. I was very proud of the mobile nav hamburger animation, it made its way to <code>v1</code> as well, and even <code>v2</code>. Originally, the projects section was a placeholder, but I decided to put some old-school projects here, including high-school assignments and my first webpage.",
     },
     concentration: {
       title: "Concentration",
       date: "2014",
       description:
-        "Originally built in Khan Academy for my Grade 10 Computer Sciences course, which I only took because I thought it was robotics (but I was mistaken). I remember choosing the colours because of Portal 2, and the idea for gameplay probably came from Piano Tiles or Tap Tap Revenge, idk.",
-      sourceSite: "/",
+        "Originally built in Khan Academy for my first CS course in Grade 10 (which I only took because I thought that meant robotics). I remember choosing the colours because of Portal 2, and the idea for gameplay probably came from Piano Tiles or Tap Tap Revenge, idk.",
+      sourceSite: "https://v0.leander.xyz/#portfolio",
     },
     inversion: {
       title: "Inversion",
       date: "2015",
       description:
         "I was so proud of the graphics in this one, trying to make explosions and space ships out of primitive shapes. It's still fun to play, and the style has something to it. There's a bug where you can get over 100% accuracy, but the more I think about it, it might be a feature.",
-      sourceSite: "/",
+      sourceSite: "https://v0.leander.xyz/#portfolio",
     },
     rainbow: {
       title: "Rainbow Roadblock",
       date: "2015",
       description:
-        "Pretty simple game, but I remember finding it fun to create this ramping difficulty, and an ending at 150 points! Two control schemes as well, I personally like keyboard more but mouse is definitely easier. I remember I got a very good mark for this as my Grade 10 CPT submission.",
-      sourceSite: "/",
+        "Pretty simple game, but I remember finding it fun to create this ramping difficulty, and an ending at 150 points! Two control schemes as well, I personally like keyboard more but mouse is definitely easier. IIRC this did pretty well as a culminating project in Grade 10.",
+      sourceSite: "https://v0.leander.xyz/#portfolio",
     },
     tribute: {
       title: "Build a Tribute Page",
       date: "2018",
       description:
-        "So the assignment was to make a tribute page, but I had a color scheme in mind and so I picked some characters from Hotline Miami. Re-evaluating that decision today, I can see how having a tribute page to some violent murderous characters from an arcade-y video game might make me look, but the CSS is still cool.",
-      sourceSite: "/",
+        "So the assignment was to make a tribute page, but I had a color scheme in mind and so I picked some characters from Hotline Miami. Re-evaluating that decision today, I can see how having a tribute page to violent, creepy characters might make me look, but the CSS is still sweet.",
+      sourceSite: "https://v0.leander.xyz/#portfolio",
     },
     v1: {
       title: "It's (a)LIVE",
       date: "2018",
       description:
-        "The first time any of my code had been deployed to the internet. Very janky and had many anti-patterns, but it worked! PHPMyAdmin, cPanel, and FileZilla were all new to me, but that's what it took on SiteGround at the time. Pretty cool how much it built up from v0.",
+        "The first time any of my code had been deployed to the internet. Very janky and had many anti-patterns, but it worked! <i>PHPMyAdmin</i>, <i>cPanel</i>, and <i>FileZilla</i> were all new to me, but that's what was available on the webhost I paid for at the time. Pretty cool how much it built up from v0.",
     },
     v1_projects: {
-      title: "Legacy freeCodeCamp Gallery",
+      title: "Earliest Gallery",
       date: "2018",
       description:
-        "All a part of the legacy freeCodeCamp curriculum. As 'apps', they're totally a mixed bag, but do remember that I never started the next project without being proud of the the previous. They're probably a large reason for my comprehension of CSS.",
+        "All a part of the legacy freeCodeCamp curriculum. As applications, they're totally a mixed bag. Probably not all that useful, but I put a lot of effort into them at the time. I never started the next project until I was proud of the previous. Figuring these out taught me quite a bit of CSS.",
     },
     v1_blog: {
       title: "Dear Diary",
@@ -56,69 +56,69 @@ function attachPlaque() {
       title: "Accordions",
       date: "2018",
       description:
-        "Weird way to display sections of a resume, probably a reason it didn't make it into v2. Having to unfold each section feels a bit odd by I do like the transition a fair bit! Content-wise, I find it pretty embarrassing, but it did get me my first development job!",
-    },
-    calculator: {
-      title: "Calculator",
-      date: "2018",
-      description:
-        "I guess it kinda works, but only to a few decimal places and it has a limit for the number of digits. I tried to use this one time and my numbers were too large, so I think it's never been used practically ever. Why is there an off button?",
-      sourceSite: "/",
-    },
-    fuelgauge: {
-      title: "Productivity Fuel Gauge",
-      date: "2018",
-      description:
-        "A pomodoro timer with a bit of a theme to it. I do actually like the intent behind this design though, it makes it obvious how much 'energy' you have left to spend in a recognizable format. Good job me.",
-      sourceSite: "/",
-    },
-    liveontwitch: {
-      title: "Live on Twitch",
-      date: "2018",
-      description:
-        "So this really doesn't work. There used to be a public passthrough for the entire Twitch API, but now it's limited to a handful of accounts so adding streamers is hit or miss. Was very proud of this originally though, first time using real data.",
-      sourceSite: "/",
+        "Weird way to display sections of a resume, probably a reason it didn't make it into <code>v2</code>. Having to unfold each section kinda sucks, but I do like the transition. Content-wise, it's a mixed bag, although I'm fairly certain it landed me my first dev job.",
     },
     quotes: {
-      title: "TinDirectory",
+      title: "tinDirectory",
       date: "2018",
       description:
-        "IIRC this was the first real JS project I had done on freeCodeCamp, and it was literally just to make a random quote appear. I thought I'd do extra credit and have it cycle even more data, like names, locations and backgrounds. All in the theme of an tinder, an app I've never used that might have spawned a venture-backed industry eroding the modern relationship.",
-      sourceSite: "/",
+        "IIRC this was the first real JS project I had done on freeCodeCamp, and it was just supposed to make random quotes appear from some list. I thought I'd do extra credit and have it cycle even more data, like names, locations and backgrounds. All in the theme of an <i>Tinder</i>, an app I've never used and never will.",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-7",
     },
-    simon: {
-      title: "Simon Says",
+    wikiview: {
+      title: "WikiView",
       date: "2018",
       description:
-        "Revisiting this to put in the museum, it just appears like a boring Simon Says app to start. Mashing 'Remix' a few times, and then 'Spin', and it's kinda cute. I really like older projects like these because no one asked me to do this weird stuff, it's just the result of 'playing' with 'toys'.",
-      sourceSite: "/",
-    },
-    tictactoe: {
-      title: "Tic Tac Toe",
-      date: "2018",
-      description:
-        "Cleaner design than any of my other apps at the time, and to be honest -- still impressive! I know it's just an algorithm, but the bot actually can't be beat! The difficulty selections are tangible, the UI is pleasant and the you can even play against a human.",
-      sourceSite: "/",
+        "I'm still impressed with the level of free access to the Wikipedia API. It's been ages and this thing still works as I built it, which means all kinds of bugs I never noticed back then. Regardless, I do remember feeling very proud of the UI and animation, it's still pretty clean.",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-6",
     },
     weather: {
       title: "Outside from the Inside",
       date: "2018",
       description:
         "It's super simple, but I still find this one charming with its changing backgrounds, and excess information. I can tell I was desperate to be adding stuff to make it cool rather than just display a number on screen after the user 'accepts' the location request.",
-      sourceSite: "/",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-5",
     },
-    wikiview: {
-      title: "WikiView",
+    liveontwitch: {
+      title: "Live on Twitch",
       date: "2018",
       description:
-        "I'm still impressed with the level of free access to the Wikipedia API. It's been ages and this thing still works as I built it, which means all kinds of bugs I never noticed back then. Regardless I do remember feeling very proud of the UI, and that opening animation is pretty clean.",
-      sourceSite: "/",
+        "So this really doesn't work. There used to be a public passthrough for the entire Twitch API, but now it's limited to a handful of accounts so adding streamers is hit or miss. Was very proud of this though, it was neat seeing API data about stuff I'm semi-familiar with!",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-4",
+    },
+    calculator: {
+      title: "Calculator",
+      date: "2018",
+      description:
+        "I guess it kinda works, but only to a few decimal places and digits. The one time I tried to use this intentionally, my numbers were too large, so I think to this day, it's never done a calculation with any real-world impact. Also, why is there an off button?",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-3",
+    },
+    fuelgauge: {
+      title: "Productivity Fuel Gauge",
+      date: "2018",
+      description:
+        "A pomodoro timer with a bit of a theme to it. I do actually like the intent behind this design though, it makes it obvious how much 'energy' you have left to spend in a recognizable format. Not really my kind of tool, but it's still charming!",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-2",
+    },
+    simon: {
+      title: "Simon Says",
+      date: "2018",
+      description:
+        "Revisiting this to put in the museum, it just appears like a boring Simon Says app to start. Mashing 'Remix' a few times, and then 'Spin', and it's kinda cute. I really like older projects like these because no one asked me to do this weird stuff, it's just the creative result of learning new things and throwing it all together.",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-1",
+    },
+    tictactoe: {
+      title: "Tic Tac Toe",
+      date: "2018",
+      description:
+        "Cleaner design than any of my other apps at the time, and to be honest -- still impressive! I know it's just an algorithm, but the <i>Impossible AI</i> actually can't be beat! The difficulty selections are tangible, the colour picker is a nice touch and the you can even play against a human.",
+      sourceSite: "https://v1.leander.xyz/projects/#proj-0",
     },
     v2: {
       title: "v2",
       date: "2019",
       description:
-        "I was so SO proud of this version. When I published it I remember trying to show it off absolute everywhere. All the pages had a vibe, and I remember sketching out the idea, I adored the way it shifted colours. I even added an easter egg! 🥚",
+        "I was immensely proud of this version. When I published it, I remember trying to show it off absolute everywhere that let me add a website to my profile. All the pages had a vibe, and I remember sketching out the idea, I adored the way it shifted colours. I even added an easter egg! 🥚",
     },
     v2_about: {
       title: "All About [Someone Else]",
@@ -209,7 +209,7 @@ function attachPlaque() {
       title: "Sick Fits",
       date: "2019",
       description:
-        "The vibe of this course site definitely influenced the aesthetic for v2 of my personal site. It was the last project before I started working on it, and I remember thinking the bold red text background was extremely eye-catching. Skews and high-contrast as well!",
+        "The vibe of this course site definitely influenced the aesthetic for <code>v2</code> of my personal site. It was the last project before I started working on it, and I remember thinking the bold red text background was extremely eye-catching. Skews and high-contrast as well!",
       sourceSite: "https://v2.leander.xyz/projects/sick-fits/",
     },
     "stacker-news": {
